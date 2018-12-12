49fe10a11bf4d9b9630d196e25887047e76c5fbd
maryashka-kudryashka <Maryashka3@gmail.com>
2018-12-12 12:10:59 +0200

    server updates

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const db = require("../db");
 
 users = [
-  { name: "Arsen", surname: "Senkivskiy" },
-  { name: "Maryana", surname: "Mysak" }
-];
+  { name: "Arsen", surname: "Senkivskiy", publicKey: 01, balance: 100 },
+  { name: "Maryana", surname: "Mysak", publicKey: 02, balance: 20 },
+  { name: "Olya", surname: "Bakay", publicKey: 03, balance: 1000 },
+  { name: "Oles", surname: "Kozak", publicKey: 04, balance: 300 },
+  { name: "Yaryna", surname: "Korduba", publicKey: 05, balance: 50 },
+  { name: "Olenka", surname: "Skibinska", publicKey: 06, balance:7 }
+]
+
 router.get("/allusers", async (req, res) => {
-  console.log("ho-hey");
   res.send(users);
 });
 
--- src/components/Balances.js
+++ src/components/Balances.js
@@ -5,12 +5,30 @@ import { getAllUsers } from "../reducers"
 import { compose } from "ramda"
 import { lifecycle } from "recompose"
 
+import block from "../helpers/BEM"
+import "../styles/Balances.scss"
+
+const b = block("Balances")
+
 const Balances = ({ users }) => (
-  <div>
+  <div className={b()}>
     <div>It`s a page, where you can check balances of all students</div>
-    <span>{users}</span>
+    {users.map(el => <div className={b("student")}>
+      <label className={b("item")}>
+        Name:
+      <span> {el.name} </span>
+      </label>
+      <label className={b("item")}>
+        Surname:
+      <span>{el.surname}</span>
+      </label>
+      <label className={b("item")}>
+        Balance:
+      <span>{el.balance}</span>
+      </label>
+    </div> )}
   </div>
-);
+)
 
 export default compose(
   connect(
