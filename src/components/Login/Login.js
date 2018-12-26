e464f62fac83117ff437b7a48707aeaf2f15eaf7
yarynakorduba <korduba_y@ucu.edu.ua>
2018-12-26 21:03:13 +0200

    added styles to my transactions

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,31 +1,28 @@
 import React from "react"
 import { compose } from "ramda"
 import { withHandlers, withState } from "recompose"
-
 import { connect } from "react-redux"
 import { authenticateUser } from "../../actions/users"
 import { getCurrentUser } from "../../reducers/index"
+import block from "../../helpers/BEM"
 
-import "./Login.css"
+import "./Login.scss"
+
+const b = block("Login")
 
 const Login = ({ setEmail, setPassword, formSubmission }) => {
   return (
-    <div className={"Login"}>
-      <form className={"Login__form"} onSubmit={formSubmission}>
-        <h1 className={"Login__header"}>ucu token</h1>
+    <div className={b()}>
+      <form className={b("form")} onSubmit={formSubmission}>
+        <h1 className={b("header")}>ucu token</h1>
+        <input className={b("input")} placeholder={"email"} type={"email"} onChange={ev => setEmail(ev.target.value)} />
         <input
-          className={"Login__input"}
-          placeholder={"email"}
-          type={"email"}
-          onChange={ev => setEmail(ev.target.value)}
-        />
-        <input
-          className={"Login__input"}
+          className={b("input")}
           placeholder={"password"}
           type={"password"}
           onChange={ev => setPassword(ev.target.value)}
         />
-        <button className={"Login__button"}>login</button>
+        <button className={b("button")}>login</button>
       </form>
     </div>
   )
--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -4,6 +4,40 @@ import "./Transactions.scss"
 
 const b = block("Transactions")
 
-const Transactions = () => <div className={b()}>HERE GOES USER TRANSACTIONS</div>
+const Transactions = () => (
+  <div className={b()}>
+    <div className={b("transactions")}>
+      <h3 className={b("subheader")}>My transactions</h3>
+      <div className={b("heading")}>
+        <div className={b("col", ["first"])}>Status</div>
+        <div className={b("col", ["second"])}>Date</div>
+        <div className={b("col", ["third"])}>To/From</div>
+        <div className={b("col", ["fourth"])}>Amount</div>
+        <div className={b("col", ["fifth"])}>Issue</div>
+        <div className={b("col", ["sixth"])}>Transaction ID</div>
+      </div>
+      <div className={b("row")}>
+        <div className={b("col", ["first"])}>Status</div>
+        <div className={b("col", ["second"])}>Date</div>
+        <div className={b("col", ["third"])}>
+          To/From<div className={b("status")}>Received</div>
+        </div>
+        <div className={b("col", ["fourth"])}>Amount</div>
+        <div className={b("col", ["fifth"])}>Issue</div>
+        <div className={b("col", ["sixth"])}>Transaction ID</div>
+      </div>
+      <div className={b("row")}>
+        <div className={b("col", ["first"])}>Status</div>
+        <div className={b("col", ["second"])}>Date</div>
+        <div className={b("col", ["third"])}>
+          To/From<div className={b("status")}>Received</div>
+        </div>
+        <div className={b("col", ["fourth"])}>Amount</div>
+        <div className={b("col", ["fifth"])}>Issue</div>
+        <div className={b("col", ["sixth"])}>Transaction ID</div>
+      </div>
+    </div>
+  </div>
+)
 
 export default Transactions
